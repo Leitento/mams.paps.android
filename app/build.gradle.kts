5cb91bfd898543cae3cec9b09a523e5441cf39b9
Nikita Obrekht <obrekht@gmail.com>
2023-11-17 01:03:47 +0200

    Use ViewBindingPropertyDelegate library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.viewbindingdelegate)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
--- app/src/main/java/com/mams/paps/MainActivity.kt
+++ app/src/main/java/com/mams/paps/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.mams.paps
 
-import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AnticipateInterpolator
-import android.view.animation.LinearInterpolator
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.mams.paps.databinding.ActivityMainBinding
-import com.mams.paps.util.viewBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private val binding by viewBinding(ActivityMainBinding::inflate)
+    private val binding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
 
         enableEdgeToEdge()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
