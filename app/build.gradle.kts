1c1d7d0027d20ad8a6c419a02bb53d90252a5626
Nikita Obrekht <obrekht@gmail.com>
2023-12-02 17:45:40 +0200

    Add Navigation Safe Args

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.navigation.safeargs)
 }
 
 android {
@@ -35,15 +36,12 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-
     implementation(libs.core.ktx)
     implementation(libs.core.splashscreen)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.bundles.navigation)
     implementation(libs.viewbindingdelegate)
 
     testImplementation(libs.junit)
