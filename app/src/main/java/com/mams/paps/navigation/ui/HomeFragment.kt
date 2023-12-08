c05847cc7928534c607400d37c71aa3b90d01143
Nikita Obrekht <obrekht@gmail.com>
2023-12-08 23:01:19 +0200

    Move home, add and profile fragments to navigation

--- app/src/main/java/com/mams/paps/navigation/ui/HomeFragment.kt
+++ app/src/main/java/com/mams/paps/navigation/ui/HomeFragment.kt
@@ -1,10 +1,10 @@
-package com.mams.paps.ui.bottom_bar
+package com.mams.paps.navigation.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.mams.paps.R
 
 class HomeFragment : Fragment() {
