085b5786c038d972a64d8a24ded8f05f76dc5b79
Nikita Obrekht <obrekht@gmail.com>
2023-12-21 01:29:44 +0200

    Clear backstack on logout

--- app/src/main/java/com/mams/paps/main/ui/MainActivity.kt
+++ app/src/main/java/com/mams/paps/main/ui/MainActivity.kt
@@ -181,7 +181,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     }
 
                     UiEvent.NavigateToAuth -> {
-                        val intent = Intent(this@MainActivity, AuthActivity::class.java)
+                        val intent = Intent(this@MainActivity, AuthActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
                         startActivity(intent)
                         finish()
                     }
