a8370839c7c1ca62c013dfc0af146d823f4d0bce
Nikita Obrekht <obrekht@gmail.com>
2024-01-09 18:05:25 +0300

    Add user profile screen (#16)

---
+++ app/src/main/java/com/mams/paps/navigation/ui/profile/ProfileMenuAdapter.kt
@@ -0,0 +1,55 @@
+package com.mams.paps.navigation.ui.profile
+
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.mams.paps.databinding.ItemProfileMenuBinding
+
+class ProfileMenuAdapter(
+    private val clickListener: ProfileMenuItemClickListener
+) : RecyclerView.Adapter<ProfileMenuViewHolder>() {
+
+    var items: List<ProfileMenuItem> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileMenuViewHolder {
+        val binding = ItemProfileMenuBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ProfileMenuViewHolder(binding, clickListener)
+    }
+
+    override fun onBindViewHolder(holder: ProfileMenuViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+}
+
+class ProfileMenuViewHolder(
+    val binding: ItemProfileMenuBinding,
+    private val clickListener: ProfileMenuItemClickListener
+) : ViewHolder(binding.root) {
+
+    fun bind(item: ProfileMenuItem) {
+        val context = itemView.context
+
+        with(binding) {
+            itemView.setOnClickListener {
+                clickListener.onClick(item, adapterPosition)
+            }
+
+            title.setText(item.titleResId)
+            icon.setImageResource(item.iconResId)
+
+            val iconBackgroundColor = context.getColor(item.backgroundColorResId)
+            iconBackground.backgroundTintList = ColorStateList.valueOf(iconBackgroundColor)
+        }
+    }
+}
---
+++ app/src/main/java/com/mams/paps/navigation/ui/profile/ProfileMenuItem.kt
@@ -0,0 +1,12 @@
+package com.mams.paps.navigation.ui.profile
+
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.mams.paps.R
+
+class ProfileMenuItem(
+    @StringRes val titleResId: Int,
+    @DrawableRes val iconResId: Int,
+    @ColorRes val backgroundColorResId: Int = R.color.red
+)
