b033d920b025f51353b366d4373f19dd3a281ca0
Nikita Obrekht <obrekht@gmail.com>
2024-01-11 17:33:18 +0200

    Make onboarding screen edge-to-edge

--- app/src/main/java/com/mams/paps/onboarding/ui/OnboardingActivity.kt
+++ app/src/main/java/com/mams/paps/onboarding/ui/OnboardingActivity.kt
@@ -1,10 +1,17 @@
 package com.mams.paps.onboarding.ui
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.mams.paps.R
 import com.mams.paps.auth.ui.AuthActivity
@@ -35,7 +42,27 @@ class OnboardingActivity : AppCompatActivity(R.layout.activity_onboarding) {
     private var currentStageIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
         super.onCreate(savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            view.updatePadding(
+                top = insets.top,
+                bottom = insets.bottom
+            )
+
+            windowInsets
+        }
 
         binding.buttonToNextScreen.setOnClickListener {
             if (currentStageIndex < onboardingStages.size - 1) {
